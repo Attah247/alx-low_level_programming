9628c8cb980142687f97526be878b61e7fccbc25
Attah247 <attahsamuel247@gmail.com>
2023-02-16 18:56:23 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that creates a new line at the end
+ * Return: 0 (success
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
