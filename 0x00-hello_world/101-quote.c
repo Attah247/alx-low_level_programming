7a41437611c2e2ef5e887eb85a53814a9a1b3b1b
Attah247 <attahsamuel247@gmail.com>
2023-02-20 18:16:29 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,13 @@
 #include <unistd.h>
 /**
  * main - A c program that prints with a command other than puts and printf
+ *
  * Return: 1 (success)
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		59);
 	return (1);
 }
