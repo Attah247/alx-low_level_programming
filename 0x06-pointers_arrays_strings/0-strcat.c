8134d7593e03e3c57991d2dfa9cd031f00261e6b
Attah247 <attahsamuel247@gmail.com>
2023-03-01 15:47:22 -0800

    task0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,13 +10,13 @@
  */
 char *strcat(char *dest, const char *src)
 {
-	int index = 0, dest_len = 0;
+	int i = 0, longi = 0;
 
-	while (dest[index++])
-		dest_len++;
+	while (dest[i++])
+		longi++;
 
-	for (index = 0; src[index]; index++)
-		dest[dest_len++] = src[index];
+	for (i = 0; src[i]; i++)
+		dest[longi++] = src[i];
 
 	return (dest);
 }
