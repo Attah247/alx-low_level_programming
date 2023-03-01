18929147adea437ef9f483b81091a9a0137ad54f
Attah247 <attahsamuel247@gmail.com>
2023-03-01 14:50:19 -0800

    task1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,23 @@
 #include "main.h"
 
 /**
- * _memcpy - Copies @n bytes from the memory area pointed
- *           to by @src into that pointed to by @dest.
- * @dest: A pointer to the memory area to copy @src into.
- * @src: The source buffer to copy characters from.
- * @n: The number of bytes to copy from @src.
+ * _strncat - Concatenates two strings using at most
+ *            an inputted number of bytes from src.
+ * @dest: The string to be appended upon.
+ * @src: The string to be appended to dest.
+ * @n: The number of bytes from src to be appended to dest.
  *
- * Return: A pointer to the destination buffer @dest.
+ * Return: A pointer to the resulting string dest.
  */
-void *_memcpy(void *dest, const void *src, size_t n)
+char *_strncat(char *dest, char *src, int n)
 {
-	unsigned int index;
-	unsigned char *destination = dest;
-	const unsigned char *source = src;
+	int index = 0, dest_len = 0;
 
-	for (index = 0; index < n; index++)
-		destination[index] = source[index];
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 }
