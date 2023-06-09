6b63e3eef8dc7bdd688a07bf9ed7b37aad329ce0
Attah247 <attahsamuel247@gmail.com>
2023-06-09 16:53:44 -0700

    my commit message for the argc task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: Number of arguments passed to the program
+ * @argv: Array of strings containing arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+
+	return (0);
+}
