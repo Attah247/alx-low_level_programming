8d706f8754e6739a97dd7a831251490721ebdf75
Attah247 <attahsamuel247@gmail.com>
2023-06-15 19:34:46 -0700

    my malloc commit message

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * _islower - entry point of the function
+ * @c: the integer to print
+ *
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+
+	return (c >= 'a' && c <= 'z');
+
+}
